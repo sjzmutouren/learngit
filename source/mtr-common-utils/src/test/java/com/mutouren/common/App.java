9b0e0f06d48070055745b3f84b8672a6630dc5c2
sjzmutouren <sjzmutouren@126.com>
2017-02-17 10:08:09 +0800

    Update App.java

--- source/mtr-common-utils/src/test/java/com/mutouren/common/App.java
+++ source/mtr-common-utils/src/test/java/com/mutouren/common/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("hello baby.");
 		System.out.println("hello world");
+		System.out.println("hello world，hao123");
 	}
 
 }
